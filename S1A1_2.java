aa666239af71e69ce8b1a914782b3886ccaf213b
adityavgp <gpaditya11@gmail.com>
2016-08-25 20:28:13 +0530

    Add files via upload

---
+++ S1A1_2.java
@@ -0,0 +1,23 @@
+
+
+
+public class S1A1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c= a-- - --a;
+		int g=--a;
+		int d=a--;
+		int e=a>>2;
+		int f=a&b;
+		System.out.println("c=" +c);
+		System.out.println("d=" +d);
+		System.out.println("e=" +e);
+		System.out.println("f=" +f);
+		System.out.println("g=" +g);
+
+	}
+
+}
